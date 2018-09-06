a79d1b214dd30394e839639044c8d74dcecf5ad6
archaeogeek <jo.k.cook@gmail.com>
2018-09-06 17:06:31 +0100

    Added option for showing tables with statement-level triggers

--- audit.sql
+++ audit.sql
@@ -244,8 +244,8 @@ CREATE OR REPLACE VIEW audit.tableslist AS
  SELECT DISTINCT triggers.trigger_schema AS schema,
     triggers.event_object_table AS auditedtable
    FROM information_schema.triggers
-  WHERE triggers.trigger_name::text = 'audit_trigger_row'::text
-  ORDER BY schema, auditedtable;
+    WHERE triggers.trigger_name::text IN ('audit_trigger_row'::text, 'audit_trigger_stm'::text)  
+ORDER BY schema, auditedtable;
 
 COMMENT ON VIEW audit.tableslist IS $body$
 View showing all tables with auditing set up. Ordered by schema, then table.
